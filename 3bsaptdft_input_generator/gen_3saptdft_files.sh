92175d3fe06db40c145f19bced51eff5b5de5d6c
John W. Melkumov <79213530+jwmelkumov@users.noreply.github.com>
2024-09-26 14:19:36 -0400

    Added more automation

--- 3bsaptdft_input_generator/gen_3saptdft_files.sh
+++ 3bsaptdft_input_generator/gen_3saptdft_files.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+#================================================
 # Utility script to generate all necessary files
 # for running 3SAPTDFT. User need only prepare
-# ${NAME}*.dal files, corr file, ${NAME}P.data file,
-# and trimer.info file with the following
-# scripts/programs present in the working directory:
+# *.dal files, corr file, ${NAME}P.data file, 
+# and trimer.info file with the following 
+# scripts/programs present in the working 
+# directory:
 #
-# (1) gen_trimer_cnf, (2) add_coords_to_mol.sh
+# (1) gen_trimer_cnf, (2) gen_dimer_cnf, 
+# (3) add_coords_to_mol.sh
 #
-# - John W. Melkumov
+# Author: John W. Melkumov
+#================================================
 
 MAIN_SAPT_DIR=/data/melkumov/SAPT2020.1
 
@@ -37,6 +41,15 @@ BASIS=$3
 
 # check if trimer.info file exists
 # if not throw error 'trimer.info file not found.'
+if [ -f "${NAME}P.data" ]; then
+    continue
+else
+    echo "${NAME}P.data file not found."
+    exit
+fi
+
+# check if trimer.info file exists
+# if not throw error 'trimer.info file not found.'
 if [ -f "trimer.info" ]; then
     continue
 else
@@ -53,12 +66,21 @@ else
     exit
 fi
 
-# check if dimer.cnf file exists
-# if not throw error 'dimer.cnf not found.'
-if [ -f "dimer.cnf" ]; then
+# check if gen_dimer_cnf binary exists
+# if not throw error 'gen_dimer_cnf not found.'
+if [ -f "gen_dimer_cnf" ]; then
     continue
 else
-    echo 'dimer.cnf file not found.'
+    echo 'gen_dimer_cnf binary not found.'
+    exit
+fi
+
+# check if gen_tempcoords binary exists
+# if not throw error 'gen_tempcoords not found.'
+if [ -f "gen_tempcoords" ]; then
+    continue
+else
+    echo 'gen_tempcoords binary not found.'
     exit
 fi
 
@@ -69,11 +91,21 @@ fi
 # this will generate trimer.cnf
 ./gen_trimer_cnf
 
-# create dimer input files
+# run gen_dimer_cnf (no arguments necessary)
+# with trimer.info present in working directory
+# this will generate trimer.cnf
+./gen_dimer_cnf
+
+# run gen_tempcoords (no arguments necessary)
+# with trimer.info present in working directory
+# this will generate tempcoords
+./gen_tempcoords
+
+# generate dimer *.mol files
 $MAIN_SAPT_DIR/misc/daltutil/createinputs $NAME $TYPE $BASIS
 
-# run createtrimers from $SAPTDIR/misc/daltutils/
-# this will generate trimer *.dal and *.mol files
+# run createtrimers from $MAIN_SAPT_DIR/misc/daltutil/
+# this will generate trimer *.mol files
 $MAIN_SAPT_DIR/misc/daltutil/createtrimers $NAME 0 $BASIS
 
 # run add_coords_to_mol.sh with NAME argument
@@ -81,15 +113,11 @@ $MAIN_SAPT_DIR/misc/daltutil/createtrimers $NAME 0 $BASIS
 # all 0 after createtrimers)
 ./add_coords_to_mol.sh $NAME
 
-# check if ${NAME}C.dal file exists
-# if not, copy ${NAME}A.dal to C
-# as there is no difference
-if [ -f "${NAME}C.dal" ]; then
-    cp "${NAME}A.dal" "${NAME}C.dal" 
-    continue
-else
-    exit
-fi
+# copy ${NAME}A.dal for the
+# monomers, dimers, and trimer
+for X in A B C AB BC AC ABC; do
+    cp "${NAME}A.dal" "${NAME}${X}.dal"
+done
 
 for X in A B C AB BC AC ABC; do
     mv "modified_${NAME}${X}.mol" "${NAME}${X}.mol" 
---
+++ 3bsaptdft_input_generator/src/gen_tempcoords.f95
@@ -0,0 +1,66 @@
+program gen_tempcoords
+! Program used to generate input to 3SAPTDFT
+! user must prepare trimer.info file with
+! and program will generate tempcoords file
+! - John Melkumov 2/21/2024
+    implicit none
+    character(len=100) :: monomer_label(3) = ['MONOMER_A', 'MONOMER_B', &
+                                              'MONOMER_C']
+    integer, parameter :: maxsites = 10000
+    character(len=100) :: line
+    character(len=100) :: system_name
+    integer :: num_atoms_total
+    real, allocatable :: x(:,:), y(:,:), z(:,:)
+    real, allocatable :: nuc_charge(:,:), atm_mass(:,:)
+    character(len=4), allocatable :: atom_name(:,:)
+    integer :: num_atoms(maxsites)
+    integer :: i,j
+
+    ! Open input file
+    open(unit=10, file='trimer.info', status='old', action='read')
+    ! Open output file
+    open(unit=40, file='tempcoords', status='replace', action='write')
+
+    ! Read system name and number of atoms
+    read(10, *) system_name
+    read(10, *) num_atoms_total
+   
+    allocate( atom_name(3,maxsites), x(3,maxsites), y(3,maxsites), z(3,maxsites), &
+              nuc_charge(3,maxsites), atm_mass(3,maxsites) )   
+
+    ! Loop through each monomer section
+    do i = 1, 3
+        do
+            read(10, '(A100)', end=10) line
+            if (trim(line) == trim(monomer_label(i))) then
+                write(*,*) monomer_label(i)
+                exit
+            end if
+        end do
+        
+        ! Read number of atoms for each monomer
+        if (monomer_label(1) == 'MONOMER_A') then
+            read(10, *) num_atoms(i)
+            write(*,*) num_atoms(i)
+        else if (monomer_label(2) == 'MONOMER_B') then
+            read(10, *) num_atoms(i)
+        else if (monomer_label(3) == 'MONOMER_C') then
+            read(10, *) num_atoms(i)
+        end if
+        !write(*,*) 'on to the reading and writing portion ...'
+        write(*,*) ' numatoms i = ', num_atoms(i)
+        ! Read atomic coordinates, nuclear charges, and masses
+        !write(40, *) num_atoms(i)
+        do j = 1, num_atoms(i)
+            read(10, *) atom_name(i,j), x(i,j), y(i,j), z(i,j), nuc_charge(i,j), atm_mass(i,j)
+            write(*,*) 'atomnameij = ', atom_name(i,j)
+            write(40, '(A1, 3X, 3F20.13)') trim(atom_name(i,j)), x(i,j), y(i,j), z(i,j)
+        end do
+    end do
+ 
+    close(10)
+    close(40)
+
+10 continue
+
+end program gen_tempcoords
